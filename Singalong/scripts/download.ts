5291340b73fcb0372e219dbf72f8a4159ee03fc5
wpot <wpot@pointlogic.com>
2017-10-31 16:32:10 +0100

    added azure and ftp publish profile

--- Singalong/scripts/download.ts
+++ Singalong/scripts/download.ts
@@ -10,26 +10,6 @@ export async function downloadUrl(url) {
     return result;
 };
 
-var google = require('googleapis');
-var customsearch = google.customsearch('v1');
-
-var GoogleSearchEngineId = '001370558942567253574:kcolq4oco7w';
-var GoogleApiKey = 'AIzaSyAbzN37396jG-c0BpUFWzb78FRu4QnBK9M';
-
-export function search(artist, title, onResponse) {
-    customsearch.cse.list({ q: artist + ' ' + title + ' lyrics', auth: GoogleApiKey, cx: GoogleSearchEngineId }, (err, resp) => {
-        if (err) {
-            return console.log('An error occured', err);
-        }
-        // Got the response from custom search
-        console.log('Result: ' + resp.searchInformation.formattedTotalResults);
-        if (resp.items && resp.items.length > 0) {
-            console.log('First result name is ' + resp.items[0].formattedUrl);
-        }
-        downloadUrl(resp.items[0].formattedUrl).then(onResponse);
-    });
-}
-
 export async function searchAzLyrics(artist, title) {
     var res = await request('https://search.azlyrics.com/search.php?q=' + artist + '+' + title);
     var $ = cheerio.load(res);
