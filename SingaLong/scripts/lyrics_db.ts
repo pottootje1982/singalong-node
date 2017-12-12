688e530b72c007046b7bc14780cfb1d89e7fa9c1
wpot <wpot@pointlogic.com>
2017-12-12 10:13:05 +0100

    re instantiate connection in case of ECONNRESET

--- SingaLong/scripts/lyrics_db.ts
+++ SingaLong/scripts/lyrics_db.ts
@@ -1,18 +1,25 @@
 ﻿var mysql = require('mysql');
 import { Track } from './Track';
 
-var connection = mysql.createConnection({
-    //host: '192.168.178.65',
-    host: '87.195.169.201',
-    port: 3307,
-    user: 'pottootje1982',
-    password: 'Icf5uEiPRtjXD7GK',
-    database: 'singalong'
-});
+var connection = null;
 
-connection.connect();
+function createConnection() {
+    if (connection != null) connection.end();
+    connection = mysql.createConnection({
+        //host: '192.168.178.65',
+        host: '87.195.169.201',
+        port: 3307,
+        user: 'pottootje1982',
+        password: 'Icf5uEiPRtjXD7GK',
+        database: 'singalong'
+    });
+    connection.connect();
+}
+
+createConnection();
 
 // Default wait time out is 28800 seconds (8 hours), that's why we ping each hour the DB connection to avoid connection reset
+// test for ECONNRESET
 setInterval(() => {
     console.log();
     connection.query("SELECT 1", (err, rows) => {
@@ -23,24 +30,34 @@ setInterval(() => {
     });
 }, 1000 * 3600);
 
-export function query(artist, title) : Promise<Track[]> {
+function executeQuery(query : string, processResults = null) {
     return new Promise((resolve, reject) => {
-        if (title === '' || title == null) resolve(null);
-        var artistPart = artist != null ? 'Artist = ' + p(artist) + ' AND ' : '';
-        let query = 'SELECT * FROM lyrics WHERE ' + artistPart + 'Title = ' + p(title);
         //console.log('Executing query: ' + query);
         connection.query(query,
             (error, results, fields) => {
-                if (error) reject(Error(error));
-                else
-                {
-                    var tracks = [];
-                    for (let result of results)
-                        tracks.push(new Track(result.Artist, result.Title, result.Site, result.Lyrics));
-                    resolve(results.length === 0 ? null : tracks);
+                if (error) {
+                    if (error.indexOf('ECONNRESET')) { // Reestablish connection in case of connection reset
+                        console.log("Re-establishing connection");
+                        createConnection();
+                    }
+                    reject(Error(error));
                 }
+                else resolve(processResults != null ? processResults(results) : results);
             });
     });
+}
+
+export function query(artist, title): Promise<Track[]> {
+    if (title === '' || title == null) return null;
+    var artistPart = artist != null ? 'Artist = ' + p(artist) + ' AND ' : '';
+    let query = 'SELECT * FROM lyrics WHERE ' + artistPart + 'Title = ' + p(title);
+    return executeQuery(query,
+        results => {
+            var tracks = [];
+            for (let result of results)
+                tracks.push(new Track(result.Artist, result.Title, result.Site, result.Lyrics));
+            return results.length === 0 ? null : tracks;
+        });
 }
 
 export async function queryTrack(track: Track) {
@@ -61,16 +78,9 @@ export async function queryTrack(track: Track) {
 }
 
 export function insert(track: Track, lyrics: string) {
-    return new Promise((resolve, reject) => {
-        let query = "INSERT INTO lyrics (Artist,Title,Site,Lyrics) " +
-            'VALUES("' + track.artist + '", "' + track.title + '", "' + track.site + '", ' + formatLyrics(lyrics) + ')';
-        //console.log('Executing query: ' + query);
-        connection.query(query,
-            (error, results, fields) => {
-                if (error) reject(Error(error));
-                else resolve(results);
-            });
-    });
+    let query = "INSERT INTO lyrics (Artist,Title,Site,Lyrics) " +
+        'VALUES("' + track.artist + '", "' + track.title + '", "' + track.site + '", ' + formatLyrics(lyrics) + ')';
+    return executeQuery(query);
 }
 
 function formatLyrics(lyrics: string) {
@@ -82,17 +92,10 @@ function p(value: string) {
 }
 
 function updateInternal(track: Track, lyrics: string) {
-    return new Promise((resolve, reject) => {
-        let query = "UPDATE lyrics " +
-            'SET Site=' + p(track.site) + ', Lyrics=' + formatLyrics(lyrics) + ' ' +
-            'WHERE Artist=' + p(track.artist) + ' AND Title=' + p(track.title);
-        //console.log('Executing query: ' + query);
-        connection.query(query,
-            (error, results, fields) => {
-                if (error) reject(Error(error));
-                else resolve(results);
-            });
-    });
+    let query = "UPDATE lyrics " +
+        'SET Site=' + p(track.site) + ', Lyrics=' + formatLyrics(lyrics) + ' ' +
+        'WHERE Artist=' + p(track.artist) + ' AND Title=' + p(track.title);
+    return executeQuery(query);
 }
 
 export async function update(track: Track, lyrics: string) {
@@ -102,14 +105,7 @@ export async function update(track: Track, lyrics: string) {
 }
 
 export function remove(track: Track) {
-    return new Promise((resolve, reject) => {
-        let query = "DELETE FROM lyrics " +
-            'WHERE Artist=' + p(track.artist) + ' AND Title=' + p(track.title);
-        console.log('Executing query: ' + query);
-        connection.query(query,
-            (error, results, fields) => {
-                if (error) reject(Error(error));
-                else resolve(results);
-            });
-    });
+    let query = "DELETE FROM lyrics " +
+        'WHERE Artist=' + p(track.artist) + ' AND Title=' + p(track.title);
+    return executeQuery(query);
 }
