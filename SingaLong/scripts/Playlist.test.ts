f1f7a6b244f7663ba42a6022c563a19ca40cc9f2
wpot <wpot@pointlogic.com>
2017-12-20 13:57:12 +0100

    added tests for playlist

---
+++ SingaLong/scripts/Playlist.test.ts
@@ -0,0 +1,21 @@
+﻿import assert = require('assert');
+import {Playlist} from "./Playlist"
+
+describe("Spotify API", async () => {
+    it("Empty Textual playlist to tracks",
+        () => {
+            var playlist = Playlist.textualPlaylistToPlaylist('');
+            assert.equal(playlist.items.length, 0);
+        });
+
+    it("Textual playlist to tracks",
+        () => {
+            var playlist = Playlist.textualPlaylistToPlaylist('1793 George Harrison - Give me Love\n' +
+                "Beatles -Yellow Submarine");
+            assert.equal(playlist.items.length, 2);
+            assert.equal('1793 George Harrison', playlist[0].artist);
+            assert.equal("Give me Love", playlist[0].title);
+            assert.equal('Beatles', playlist[1].artist);
+            assert.equal("Yellow Submarine", playlist[1].title);
+        });
+});
