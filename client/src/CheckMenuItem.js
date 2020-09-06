6350c93f6ab9ff7eb96d7f24f0dfe7064a4b65f7
Wouter Pot <wouter.pot@nielsen.com>
2020-09-06 13:03:36 +0200

    Fixed function components cannot be given refs

--- client/src/CheckMenuItem.js
+++ client/src/CheckMenuItem.js
@@ -4,18 +4,22 @@ import { MenuItem, ListItemIcon, ListItemText } from '@material-ui/core'
 
 import { CheckBoxOutlineBlank, CheckBoxOutlined } from '@material-ui/icons'
 
-export default function CheckMenuItem({ setter, checked, name, close }) {
-  function onCheckClick() {
-    setter(!checked)
-    close()
-  }
+const CheckMenuItem = React.forwardRef(
+  ({ setter, checked, name, close }, ref) => {
+    function onCheckClick() {
+      setter(!checked)
+      close()
+    }
 
-  return (
-    <MenuItem onClick={onCheckClick}>
-      <ListItemIcon>
-        {checked ? <CheckBoxOutlined /> : <CheckBoxOutlineBlank />}
-      </ListItemIcon>
-      <ListItemText primary={name} />
-    </MenuItem>
-  )
-}
+    return (
+      <MenuItem ref={ref} onClick={onCheckClick}>
+        <ListItemIcon>
+          {checked ? <CheckBoxOutlined /> : <CheckBoxOutlineBlank />}
+        </ListItemIcon>
+        <ListItemText primary={name} />
+      </MenuItem>
+    )
+  }
+)
+
+export default CheckMenuItem
