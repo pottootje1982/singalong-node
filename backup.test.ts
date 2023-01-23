2bc671e4011eae79a3d436dc663df1d3160bd57c
Wouter Pot <wouter@baseflow.com>
2023-01-23 17:11:53 +0100

    reformat

--- backup.test.ts
+++ backup.test.ts
@@ -1,44 +1,44 @@
-import { Track, simpleTrack } from './client/src/track'
+import { Track, simpleTrack } from './client/src/track';
 
 xdescribe('database queries', () => {
-  let lyrics
+  let lyrics;
 
   beforeAll(async () => {
-    lyrics = global.lyricsTable
-  })
+    lyrics = global.lyricsTable;
+  });
 
   afterAll(async () => {
-    await lyrics.close()
-  })
+    await lyrics.close();
+  });
 
   it('find duplicates', () => {
-    const tracks = require('./backup.json').map((t) => Track.copy(t))
+    const tracks = require('./backup.json').map((t) => Track.copy(t));
     for (const track of tracks) {
       const duplicates = tracks.filter(
         (t) => t.id === track.id && t.id !== '' && t.id !== 'NULL' && t.id
-      )
+      );
       if (duplicates.length > 1) {
-        console.log(duplicates) //duplicates.map((t) => t.toString()))
+        console.log(duplicates); //duplicates.map((t) => t.toString()))
       }
     }
-  })
+  });
 
   it('finds wrong ids', async () => {
     const tracks = (
       await lyrics.lyricsTable.find({ id: { $regex: /^"/ } })
-    ).map(Track.copy)
+    ).map(Track.copy);
     for (const track of tracks) {
       //await lyricsDb.remove(track)
-      console.log(track.toString())
+      console.log(track.toString());
     }
-  })
+  });
 
   it('removes dire straits - news', async () => {
-    let track = await lyrics.queryTrack(simpleTrack('Dire Straits', 'News'))
-    await lyrics.remove(track)
-    track = await lyrics.queryTrack(simpleTrack('Dire Straits', 'News'))
-    console.log(track)
-  })
+    let track = await lyrics.queryTrack(simpleTrack('Dire Straits', 'News'));
+    await lyrics.remove(track);
+    track = await lyrics.queryTrack(simpleTrack('Dire Straits', 'News'));
+    console.log(track);
+  });
 
   it('deletes track', async () => {
     await lyrics.lyricsTable.deleteOne({
@@ -46,6 +46,6 @@ xdescribe('database queries', () => {
       artist: 'Otis Redding',
       title: '',
       site: 'Genius',
-    })
-  })
-})
+    });
+  });
+});
